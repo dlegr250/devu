f7f9fbf8ed8ea6eca4500c0a13dafb32b4bdf447
Dan LeGrand <dan.legrand@protonmail.com>
2024-05-06 14:41:11 -0400

    Initial commit

---
+++ lib/devu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "devu/version"
+
+module Devu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/devu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Devu
+  VERSION = "0.1.0"
+end
